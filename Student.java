7437b5f546358645f42e34164e0e9071dec4fda1
PaulVoron <PaulVoron@users.noreply.github.com>
2018-03-22 01:43:08 +0200

    Update Student.java

--- Student.java
+++ Student.java
@@ -32,5 +32,5 @@ public class Student extends Human {
 		return "Student [surname=" + this.getSurname() + ", name=" + this.getName() + ", sex=" + this.isSex() + ", age="
 				+ this.getAge() + ", progress=" + progress + "]";
 	}
-
+ 
 }
